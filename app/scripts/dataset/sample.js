cd5c5d5e196f6622ec3fb5339ab94c6e04c767a6
modom3 <markusodom11@gmail.com>
2014-05-31 16:02:08 -0400

    Updated sample dataset to include diabetes
    Updated sample dataset to include diabetes

--- app/scripts/dataset/sample.js
+++ app/scripts/dataset/sample.js
@@ -2,40 +2,79 @@
 var sampleSet = {
 	hypertension: {
 		daterange1988_1994: {
-			totalAgeAdjusted:25.5,
-			totalCrude:24.1
+			totalAgeAdjusted: 25.5,
+			totalCrude: 24.1
 		},
-		daterange1999_2000:{
-			totalAgeAdjusted:30.0,
-			totalCrude:28.9
+		daterange1999_2000: {
+			totalAgeAdjusted: 30.0,
+			totalCrude: 28.9
 		},
-		daterange2001_2002:{
-			totalAgeAdjusted:29.7,
-			totalCrude:28.9
+		daterange2001_2002: {
+			totalAgeAdjusted: 29.7,
+			totalCrude: 28.9
 
 		},
-		daterange2003_2004:{
-			totalAgeAdjusted:32.1,
-			totalCrude:32.5
+		daterange2003_2004: {
+			totalAgeAdjusted: 32.1,
+			totalCrude: 32.5
 
 		},
-		daterange2005_2006:{
-			totalAgeAdjusted:30.5,
-			totalCrude:31.7
+		daterange2005_2006: {
+			totalAgeAdjusted: 30.5,
+			totalCrude: 31.7
 
 		},
-		daterange2007_2008:{
-			totalAgeAdjusted:31.2,
-			totalCrude:32.6
+		daterange2007_2008: {
+			totalAgeAdjusted: 31.2,
+			totalCrude: 32.6
 
 		},
-		daterange2009_2010:{
-			totalAgeAdjusted:30.0,
-			totalCrude:31.9
+		daterange2009_2010: {
+			totalAgeAdjusted: 30.0,
+			totalCrude: 31.9
 		},
-		daterange2011_2012:{
-			totalAgeAdjusted:30.0,
-			totalCrude:32.5
+		daterange2011_2012: {
+			totalAgeAdjusted: 30.0,
+			totalCrude: 32.5
 		}
 	}
-}
+	, diabetes: {
+		daterange1988_1994: {
+			totalAgeAdjusted: 9.1,
+			totalCrude: 8.4
+		},
+		daterange1999_2000: {
+			totalAgeAdjusted: 9.0,
+			totalCrude: 8.5
+		},
+		daterange2001_2002: {
+			totalAgeAdjusted: 10.5,
+			totalCrude: 10.1
+
+		},
+		daterange2003_2004: {
+			totalAgeAdjusted: 10.8,
+			totalCrude: 10.8
+
+		},
+		daterange2005_2006: {
+			totalAgeAdjusted: 10.4,
+			totalCrude: 10.7
+
+		},
+		daterange2007_2008: {
+			totalAgeAdjusted: 11.5,
+			totalCrude: 11.9
+
+		},
+		daterange2009_2010: {
+			totalAgeAdjusted: 11.5,
+			totalCrude: 12.0
+		},
+		daterange2011_2012: {
+			totalAgeAdjusted: 0,
+			totalCrude: 0
+		}
+	}
+};
+
