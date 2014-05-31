510864bcb62e40728bf6d6386e9ab6f4248835a2
modom3 <markusodom11@gmail.com>
2014-05-31 15:47:31 -0400

    Adding Sample data set
    Including data from CDC about hypertension.

---
+++ app/scripts/dataset/sample.js
@@ -0,0 +1,41 @@
+//Sample set
+var sampleSet = {
+	hypertension: {
+		daterange1988_1994: {
+			totalAgeAdjusted:25.5,
+			totalCrude:24.1
+		},
+		daterange1999_2000:{
+			totalAgeAdjusted:30.0,
+			totalCrude:28.9
+		},
+		daterange2001_2002:{
+			totalAgeAdjusted:29.7,
+			totalCrude:28.9
+
+		},
+		daterange2003_2004:{
+			totalAgeAdjusted:32.1,
+			totalCrude:32.5
+
+		},
+		daterange2005_2006:{
+			totalAgeAdjusted:30.5,
+			totalCrude:31.7
+
+		},
+		daterange2007_2008:{
+			totalAgeAdjusted:31.2,
+			totalCrude:32.6
+
+		},
+		daterange2009_2010:{
+			totalAgeAdjusted:30.0,
+			totalCrude:31.9
+		},
+		daterange2011_2012:{
+			totalAgeAdjusted:30.0,
+			totalCrude:32.5
+		}
+	}
+}
